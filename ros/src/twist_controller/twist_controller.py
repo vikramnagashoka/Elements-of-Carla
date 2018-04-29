92736a632b35c7b0597ad083a9b77f6d9de02f2a
pechyony <pechyony@outlook.com>
2018-04-28 18:32:56 -0700

    fixing small bugs in waypoint_updater and dbw

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -34,7 +34,7 @@ class Controller(object):
 	self.last_time = rospy.get_time()
 
 
-    def control(self, control_vel, dbw_enabled, linear_vel, angular_vel):
+    def control(self, current_vel, linear_vel, angular_vel, dbw_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
 
